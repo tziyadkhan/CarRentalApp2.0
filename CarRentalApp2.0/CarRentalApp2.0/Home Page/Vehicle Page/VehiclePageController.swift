415d6a3253ffe54e0d1dc42082aff8a566a551f1
Ziyadkhan Taghiyev <ziyadxan.tagiyev@gmail.com>
2023-10-30 11:17:27 +0400

    Commit
    Login Page

---
+++ CarRentalApp2.0/CarRentalApp2.0/Home Page/Vehicle Page/VehiclePageController.swift
@@ -0,0 +1,29 @@
+//
+//  VehiclePageController.swift
+//  CarRentalApp2.0
+//
+//  Created by Ziyadkhan on 30.10.23.
+//
+
+import UIKit
+
+class VehiclePageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
